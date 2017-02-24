27a61261504f3a4565bde7f1e1625da7a22c00f8
Julius Williams <doctorj89@gmail.com>
2017-02-24 13:40:21 -0500

    cleaned up code. added restart on inactivity

--- timer.js
+++ timer.js
@@ -8,6 +8,8 @@ var colorInterval;
 var tenthsInterval;
 var secondsInterval;
 var minutesInterval;
+var pauseTimeout;
+var timeoutSeconds = 5;
 var tenths = 00;
 var seconds = 00;
 var minutes = 00;
@@ -18,63 +20,32 @@ document.querySelector('#start').addEventListener('click', handleStartButton);
 // Start Button
 function handleStartButton() {
     if (startButton.innerText === "Start") {
-        // Setting intervals for tenths, seconds, and minutes
-        tenthsInterval = setInterval(starttenthsTimer, 100);
-        secondsInterval = setInterval(startSecondTimer, 1000);
-        minutesInterval = setInterval(startMinuteTimer, 60000);
-        colorInterval = setInterval(changeColor, 1000);
-        // Changing button text and color
+        setIntervals();
         startButton.innerHTML = "Pause";
         startButton.classList.add('btn-danger');
     } else if (startButton.innerText === "Pause") {
-        // Pausing counter
-        clearInterval(colorInterval);
-        clearInterval(tenthsInterval);
-        clearInterval(secondsInterval);
-        clearInterval(minutesInterval);
-        // Changing button text and color
+        clearIntervals();
+        // Set inactivity function to run after 15 seconds
+        pauseTimeout = setTimeout(inactivity, 15000);
         startButton.innerHTML = "Resume";
         startButton.classList.remove('btn-danger');
     } else if (startButton.innerText === "Resume") {
-        // Resuming counter
-        tenthsInterval = setInterval(starttenthsTimer, 100);
-        secondsInterval = setInterval(startSecondTimer, 1000);
-        minutesInterval = setInterval(startMinuteTimer, 60000);
-        colorInterval = setInterval(changeColor, 1000);
-        // Changing button text and color
+        setIntervals();
+        // Stopping inactivity function
+        clearTimeout(pauseTimeout, 5000);
         startButton.innerHTML = "Pause";
         startButton.classList.add('btn-danger');
     }
 }
 
 // Event listener for double click
-document.querySelector('#start').addEventListener('dblclick', clearTime);
+document.querySelector('#start').addEventListener('dblclick', clearTime);   
 
-// Clear existing time
+// Clear timer on when in action occurs in paused state
 function clearTime() {
-    if (startButton.innerText === "Pause") {
-        // Stopping counter
-        clearInterval(colorInterval);
-        clearInterval(tenthsInterval);
-        clearInterval(secondsInterval);
-        clearInterval(minutesInterval);
-        // Changing button text and color
-        startButton.innerText = "Start";
-        startButton.classList.remove('btn-danger');
-        // Resetting counters
-        tenths = 0;
-        seconds = 0;
-        minutes = 0;
-        // Resetting text
-        tenthsField.innerText = '00';
-        secondsField.innerText = '00';
-        minutesField.innerText = '00';
-        // Return color to black (default)
-        timer.style.color = '#000000';
-
-        // Pop up for counter reset
-        alert('You have reset the counter');
-    }
+    if (startButton.innerText === "Pause" ) {
+        resetCounter();
+    } 
 }
 
 // Count up tenths of seconds
@@ -125,4 +96,57 @@ function changeColor() {
     var blue = Math.round(Math.random() * 255);
     var rgbTemplate = `rgb(${red}, ${green}, ${blue})`
     timer.style.color = rgbTemplate;
+}
+
+// Stopping counter and color changing
+function clearIntervals() {
+    clearInterval(colorInterval);
+    clearInterval(tenthsInterval);
+    clearInterval(secondsInterval);
+    clearInterval(minutesInterval);
+}
+
+// Starting timer
+function setIntervals() {
+    tenthsInterval = setInterval(starttenthsTimer, 100);
+    secondsInterval = setInterval(startSecondTimer, 1000);
+    minutesInterval = setInterval(startMinuteTimer, 60000);
+    colorInterval = setInterval(changeColor, 1000);
+}
+
+// Resetting values
+function resetValues() {
+            // Resetting counters
+        tenths = 0;
+        seconds = 0;
+        minutes = 0;
+        // Resetting text
+        tenthsField.innerText = '00';
+        secondsField.innerText = '00';
+        minutesField.innerText = '00';
+        // Return color to black (default)
+        timer.style.color = '#000000';
+}
+
+// Setting seconds countdown for inactivity
+function inactivity() {
+    seconds--;
+    resetCounter();
+}
+
+// Reset counter and return button text to start
+function resetCounter() {
+        if (startButton.innerText === "Pause") {
+        alert('You have reset the counter');
+        } else if (startButton.innerText === "Resume") {
+            alert('You have been inactive for 15 seconds');
+        }
+        
+            // Stopping counter
+        clearIntervals();
+        startButton.innerText = "Start";
+        startButton.classList.remove('btn-danger');
+        resetValues();
+        // Pop up for counter reset
+
 }
